42a75c6dafe6a107bfb01552e27fe4750f5b4c3a
saccharin <chriswaful@hotmail.com>
2016-12-14 00:38:23 -0600

    Improved Abstraction
    Improved box abstraction; Improved branding;

--- November 2016/UI/Config.js
+++ November 2016/UI/Config.js
@@ -3,5 +3,5 @@
 var Config = {
 	ServiceResourceUrl : 'https://k4s57xqjx8.execute-api.us-west-2.amazonaws.com/demo/',
 	ApiKey : 'kZmJVm54gx6JK5C4Itj8H8AK36IW6LYo1p2afzZ4',
-	ChaosMonkeyOdds : .05
+	ChaosMonkeyOdds : 0.05
 };
